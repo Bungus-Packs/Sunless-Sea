a58a6a866eaa4354e3d0a82d7350cbad53e12055
Bennett Radtke <blradtke@gmail.com>
2025-03-22 01:41:25 -0500

    commit

---
+++ src/main/java/bungus/sunlesssea/Sunlesssea.java
@@ -0,0 +1,10 @@
+package bungus.sunlesssea;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sunlesssea implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/bungus/sunlesssea/client/SunlessseaClient.java
@@ -0,0 +1,10 @@
+package bungus.sunlesssea.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SunlessseaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
