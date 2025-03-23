3e100e0aa16f2113d1a6e2415c27ae1c8042a599
Bennett Radtke <blradtke@gmail.com>
2025-03-23 16:05:35 -0500

    initial commit

--- src/main/java/bungus/sunlesssea/Sunlesssea.java
+++ src/main/java/bungus/sunlesssea/Sunlesssea.java
@@ -1,10 +1,15 @@
 package bungus.sunlesssea;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Sunlesssea implements ModInitializer {
+    public static final String MOD_ID="sunlesssea";
+    public static final Logger LOGGER= LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-
+        LOGGER.info("Initializing "+MOD_ID);
     }
 }
--- src/main/java/bungus/sunlesssea/client/SunlessseaClient.java
+++ src/main/java/bungus/sunlesssea/client/SunlessseaClient.java
@@ -3,6 +3,7 @@ package bungus.sunlesssea.client;
 import net.fabricmc.api.ClientModInitializer;
 
 public class SunlessseaClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
 
